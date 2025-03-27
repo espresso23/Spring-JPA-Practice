c26080a5a3f7777cb2328bd2da3184002ed7dd82
espresso23 <113837129+espresso23@users.noreply.github.com>
2025-03-27 10:40:12 +0700

    dd

--- src/main/java/com/fpt/config/JpapracticeApplication.java
+++ src/main/java/com/fpt/config/JpapracticeApplication.java
@@ -20,7 +20,7 @@ public class JpapracticeApplication {
 	public static void openHomePage() {
 		Runtime runtime = Runtime.getRuntime();
 		try {
-			runtime.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8080/spmvc/benhnhan/list");
+			runtime.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8080/spmvc/benhnhan");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
